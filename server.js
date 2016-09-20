5b03000a4e79df2e1ed85f790b3dbb2075db1c29
unknown <l.maddalena@fiw9902.utenti.trenitalia.it>
2016-09-20 09:55:29 +0200

    updated server.js

--- server.js
+++ server.js
@@ -8,8 +8,8 @@ app.set('port', process.env.PORT || 1337);
 var http = require('http');
 
 var server = http.createServer(app).listen(app.get('port'), function() {
-    console.log('Express server starte in ' + app.get('env')
-    + ' mode listening on port ' + server.address().port + "; Press Ctrl+C to terminate. ");
+    // console.log('Express server starte in ' + app.get('env')
+    //+ ' mode listening on port ' + server.address().port + "; Press Ctrl+C to terminate. ");
 });
 
 
